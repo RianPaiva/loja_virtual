45037dc323e5ec2ffbbc576d5addfe371ea3985a
Rian Paiva <97189983+RianPaiva@users.noreply.github.com>
2023-09-18 22:04:02 -0300

    Add embed_instagram.php
    Adicionando recurso do instagram no index.php

--- application/pages/index.php
+++ application/pages/index.php
@@ -7,3 +7,24 @@ echo "INDEX.PHP";
 
 
 ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+
+   <?php include("../embed/embed_instagram.php");?>
+
+
+
+</body>
+
+</html>
