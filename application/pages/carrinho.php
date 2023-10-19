60cf9d9452e0f5d805d4b86e99b0ba4388a56d2b
gabriel lavechia <gabriel.lavechia1@gmail.com>
2023-10-18 23:12:26 -0300

    Carrinho Fix

--- application/pages/carrinho.php
+++ application/pages/carrinho.php
@@ -9,8 +9,7 @@ include("header.php");
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title> Meu Carrinho </title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link href="../css/style_carrinho.css" rel="stylesheet">
     <link href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css" rel="stylesheet" />
 
@@ -19,8 +18,12 @@ include("header.php");
 <body>
 
     <main class="container-fluid" style="border: 2px solid black">
-        <div class="row" style="border: 2px solid blue">
-            <div class="p-4 ms-4 row" style="border: 2px solid blue">MEU CARRINHO</div>
+        <div class="row meucarrinho" style="border: 2px solid blue">
+
+            <div class="p-2 ms-3 row" style="border: 2px solid blue">
+                <h2> MEU CARRINHO </h2>
+            </div>
+
             <div class="row text-center d-flex justify-content-around">
 
                 <div class="col-lg-8 ms-4" style="border: 2px solid black">
@@ -43,7 +46,9 @@ include("header.php");
                                     <div class="product">
                                         <img src="https://picsum.photos/100/120" alt="" />
                                         <div class="info">
-                                            <div class="name">Nome do produto</div>
+                                            <div class="name">
+                                                <h6>Nome do produto</h6>
+                                            </div>
                                             <div class="category">Categoria</div>
                                         </div>
                                     </div>
@@ -67,7 +72,9 @@ include("header.php");
                                     <div class="product">
                                         <img src="https://picsum.photos/100/120" alt="" />
                                         <div class="info">
-                                            <div class="name">Nome do produto</div>
+                                            <div class="name">
+                                                <h6>Nome do produto</h6>
+                                            </div>
                                             <div class="category">Categoria</div>
                                         </div>
                                     </div>
@@ -91,7 +98,9 @@ include("header.php");
                                     <div class="product">
                                         <img src="https://picsum.photos/100/120" alt="" />
                                         <div class="info">
-                                            <div class="name">Nome do produto</div>
+                                            <div class="name">
+                                                <h6>Nome do produto</h6>
+                                            </div>
                                             <div class="category">Categoria</div>
                                         </div>
                                     </div>
@@ -145,8 +154,7 @@ include("header.php");
     </main>
 
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
     </script>
 </body>
 
