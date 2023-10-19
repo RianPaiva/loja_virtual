91d525c078ad577d6f56d5fe481aec43ba2344c6
Gabriel Uruga da Silva <gurugadasilva@gmail.com>
2023-10-18 22:57:33 -0300

    Nova Versão Database

--- bd/bd_lavechia_store.sql
+++ bd/bd_lavechia_store.sql
@@ -1,87 +1,201 @@
-CREATE DATABASE bd_lavechia_store;
-USE bd_lavechia_store;
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Tempo de geração: 19-Out-2023 às 01:55
+-- Versão do servidor: 8.0.31
+-- versão do PHP: 8.0.26
 
-CREATE TABLE tb_adm ( 
-id_adm INT(11) NOT NULL AUTO_INCREMENT,
-nome VARCHAR(80) NOT NULL,
-email VARCHAR(80) NOT NULL UNIQUE,
-senha VARCHAR(32) NOT NULL,
-`status` tinyint(1) NOT NULL DEFAULT '0',
-PRIMARY KEY (id_adm)
-)ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
-/*SELECT * FROM tb_clientes*/
-
-
-CREATE TABLE tb_fornecedor(
-id_fornecedor INT(11) NOT NULL AUTO_INCREMENT,
-razao_social VARCHAR(100) NOT NULL,
-cnpj VARCHAR(14) NOT NULL UNIQUE,
-num_endereco INT(11) NOT NULL,
-telefone VARCHAR(13) NOT NULL,
-email VARCHAR(80) NOT NULL,
-`status` tinyint(1) NOT NULL DEFAULT '0',
-logradouro VARCHAR(80) NOT NULL,
-cidade VARCHAR(80) NOT NULL,
-estado CHAR(2) NOT NULL,
-complemento VARCHAR(80) NOT NULL,
-pais VARCHAR(50) NOT NULL,|
-PRIMARY KEY (id_fornecedor)
-) ENGINE=InnoDB DEFAULT CHARSET=LATIN1;
-
-/*SELECT * FROM tb_item_estoque*/
-
-CREATE TABLE tb_produto (
-id_produto INT(11) NOT NULL AUTO_INCREMENT,
-descricao VARCHAR(100) NOT NULL,
-id_fornecedor INT(11) NOT NULL,
-PRIMARY KEY (id_produto), 
-FOREIGN KEY (id_fornecedor) REFERENCES tb_fornecedor(id_fornecedor)
-) ENGINE=InnoDB DEFAULT CHARSET=LATIN1;  
- 
-
-CREATE TABLE tb_item_estoque (
-id_produto INT(11) NOT NULL AUTO_INCREMENT,
-valor_compra REAL(10,2) NOT NULL,
-dt_hr_entrada TIMESTAMP NOT NULL,
-qtd_disponivel INT(11) NOT NULL DEFAULT '0',
-PRIMARY KEY (id_produto)
-) ENGINE=InnoDB DEFAULT CHARSET=LATIN1;
-
-CREATE TABLE tb_cliente (
-id_cliente INT(11) NOT NULL AUTO_INCREMENT,
-nome VARCHAR(20) NOT NULL,
-sobrenome VARCHAR(60) NOT NULL,
-cpf VARCHAR(11) NOT NULL UNIQUE,
-celular VARCHAR(13) NOT NULL,
-telefone VARCHAR(13),
-email VARCHAR(80) NOT NULL UNIQUE,
-senha VARCHAR(32) NOT NULL,
-PRIMARY KEY (id_cliente)
-) ENGINE=InnoDB DEFAULT CHARSET=LATIN1;  
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
 
-CREATE TABLE tb_pedido (
-id_pedido INT(11) NOT NULL AUTO_INCREMENT,
-id_cliente INT(11) NOT NULL,
-preco_total REAL(10,2) NOT NULL,
-data_pedido TIMESTAMP NOT NULL,
-status_pagamento TINYINT(1) NOT NULL DEFAULT '0',
-PRIMARY KEY (id_pedido),
-FOREIGN KEY (id_cliente) REFERENCES tb_cliente(id_cliente)
-) ENGINE=InnoDB DEFAULT CHARSET=LATIN1;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
+--
+-- Banco de dados: `bd_lavechia_store`
+--
+CREATE DATABASE IF NOT EXISTS `bd_lavechia_store` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
+USE `bd_lavechia_store`;
 
-CREATE TABLE tb_item_pedido (
-id_pedido INT(11) NOT NULL,
-id_produto INT(11) NOT NULL,
-valor_venda REAL(10,2) NOT NULL,
-PRIMARY KEY (id_pedido, id_produto),
-FOREIGN KEY (id_pedido) REFERENCES tb_pedido(id_pedido),
-FOREIGN KEY (id_produto) REFERENCES tb_produto(id_produto)
-)ENGINE=InnoDB DEFAULT CHARSET=LATIN1;
+-- --------------------------------------------------------
 
-DROP TABLE tb_item_estoque
-DROP TABLE tb_item_pedido
-bd_lavechia_store
+--
+-- Estrutura da tabela `tb_cliente`
+--
 
+DROP TABLE IF EXISTS `tb_cliente`;
+CREATE TABLE IF NOT EXISTS `tb_cliente` (
+  `id_cliente` int NOT NULL AUTO_INCREMENT,
+  `nome` varchar(20) NOT NULL,
+  `sobrenome` varchar(60) NOT NULL,
+  `cpf` varchar(11) NOT NULL,
+  `genero` char(1) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `celular` varchar(13) NOT NULL,
+  `email` varchar(80) NOT NULL,
+  `senha` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `status` tinyint(1) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id_cliente`),
+  UNIQUE KEY `cpf` (`cpf`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=39 DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `tb_cliente`
+--
+
+INSERT INTO `tb_cliente` (`id_cliente`, `nome`, `sobrenome`, `cpf`, `genero`, `celular`, `email`, `senha`, `status`) VALUES
+(1, 'RIAN', 'PAIVA DA SILVA', '49043588806', '', '119863183463', 'paiva.dev@outlook.com', '$2y$10$NalUbyPlv.4dYOihhYkjPOc.ece3yjbVuIgWY6WxMEl4SONZ8kts6', 1),
+(2, 'aaaaaaa', 'aaaaaaaa', '11111111111', 'M', '11111111', 'aaaaaaa', 'aaaaa', 1),
+(24, 'aaaa', 'dddddd', '22222222222', 'M', '333333', 'ddddddd', 'cccc', 1),
+(25, 'aaaa', 'dddddd', '3', 'M', '333333', 'tttt', 'cccc', 1),
+(26, 'aaaa', 'aaaaaa', '4', 'M', '11111', 'tttttttt', 'aaaaa', 1),
+(27, 'aaaaaa', 'aaaaaa', '5', 'M', '1111', 'eeeeee', 'aaaaaa', 1),
+(29, 'aaaa', 'aaaaa', '6', 'M', '11111', 'aasss', '3333', 1),
+(36, 'aaaaa', 'aaaaaa', '7', 'M', 'eetttttttt', 'eeeeeef', 'aaaaaaa', 1),
+(38, 'gabriel', 'silva', '8', 'M', '11111111', 'g@gmail.com', 'aaaaaa', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `tb_fornecedor`
+--
+
+DROP TABLE IF EXISTS `tb_fornecedor`;
+CREATE TABLE IF NOT EXISTS `tb_fornecedor` (
+  `id_fornecedor` int NOT NULL AUTO_INCREMENT,
+  `razao_social` varchar(100) NOT NULL,
+  `cnpj` varchar(14) NOT NULL,
+  `num_endereco` int NOT NULL,
+  `telefone` varchar(13) NOT NULL,
+  `email` varchar(80) NOT NULL,
+  `status` tinyint(1) NOT NULL DEFAULT '0',
+  `logradouro` varchar(80) NOT NULL,
+  `cidade` varchar(80) NOT NULL,
+  `estado` char(2) DEFAULT NULL,
+  `complemento` varchar(80) NOT NULL,
+  `pais` varchar(50) NOT NULL,
+  PRIMARY KEY (`id_fornecedor`),
+  UNIQUE KEY `cnpj` (`cnpj`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `tb_item_estoque`
+--
+
+DROP TABLE IF EXISTS `tb_item_estoque`;
+CREATE TABLE IF NOT EXISTS `tb_item_estoque` (
+  `id_produto` int NOT NULL AUTO_INCREMENT,
+  `valor_compra` double(10,2) NOT NULL,
+  `dt_hr_entrada` timestamp NOT NULL,
+  `qtd_disponivel` int NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id_produto`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `tb_item_pedido`
+--
+
+DROP TABLE IF EXISTS `tb_item_pedido`;
+CREATE TABLE IF NOT EXISTS `tb_item_pedido` (
+  `id_pedido` int NOT NULL,
+  `id_produto` int NOT NULL,
+  `valor_venda` double(10,2) NOT NULL,
+  PRIMARY KEY (`id_pedido`,`id_produto`),
+  KEY `id_produto` (`id_produto`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `tb_pedido`
+--
+
+DROP TABLE IF EXISTS `tb_pedido`;
+CREATE TABLE IF NOT EXISTS `tb_pedido` (
+  `id_pedido` int NOT NULL AUTO_INCREMENT,
+  `id_cliente` int NOT NULL,
+  `preco_total` double(10,2) NOT NULL,
+  `data_pedido` timestamp NOT NULL,
+  `status_pagamento` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id_pedido`),
+  KEY `id_cliente` (`id_cliente`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `tb_produto`
+--
+
+DROP TABLE IF EXISTS `tb_produto`;
+CREATE TABLE IF NOT EXISTS `tb_produto` (
+  `id_produto` int NOT NULL AUTO_INCREMENT,
+  `descricao` varchar(100) NOT NULL,
+  `id_fornecedor` int NOT NULL,
+  PRIMARY KEY (`id_produto`),
+  KEY `id_fornecedor` (`id_fornecedor`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `tb_usuarios`
+--
+
+DROP TABLE IF EXISTS `tb_usuarios`;
+CREATE TABLE IF NOT EXISTS `tb_usuarios` (
+  `id_usuario` int NOT NULL AUTO_INCREMENT,
+  `nome` varchar(30) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `sobrenome` varchar(80) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `email` varchar(80) NOT NULL,
+  `senha` varchar(32) NOT NULL,
+  `status` tinyint(1) NOT NULL DEFAULT '0',
+  `nivel_acesso` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id_usuario`) USING BTREE,
+  UNIQUE KEY `email` (`email`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `tb_usuarios`
+--
+
+INSERT INTO `tb_usuarios` (`id_usuario`, `nome`, `sobrenome`, `email`, `senha`, `status`, `nivel_acesso`) VALUES
+(1, 'RIAN', ' PAIVA DA SILVA', 'paiva.dev@outlook.com', '123', 1, 1);
+
+--
+-- Restrições para despejos de tabelas
+--
+
+--
+-- Limitadores para a tabela `tb_item_pedido`
+--
+ALTER TABLE `tb_item_pedido`
+  ADD CONSTRAINT `tb_item_pedido_ibfk_1` FOREIGN KEY (`id_pedido`) REFERENCES `tb_pedido` (`id_pedido`),
+  ADD CONSTRAINT `tb_item_pedido_ibfk_2` FOREIGN KEY (`id_produto`) REFERENCES `tb_produto` (`id_produto`);
+
+--
+-- Limitadores para a tabela `tb_pedido`
+--
+ALTER TABLE `tb_pedido`
+  ADD CONSTRAINT `tb_pedido_ibfk_1` FOREIGN KEY (`id_cliente`) REFERENCES `tb_cliente` (`id_cliente`);
+
+--
+-- Limitadores para a tabela `tb_produto`
+--
+ALTER TABLE `tb_produto`
+  ADD CONSTRAINT `tb_produto_ibfk_1` FOREIGN KEY (`id_fornecedor`) REFERENCES `tb_fornecedor` (`id_fornecedor`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
