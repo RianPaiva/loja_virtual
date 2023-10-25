b894156a234729113d54a1aa96450af24f941a71
Gabriel Uruga da Silva <gurugadasilva@gmail.com>
2023-10-24 21:21:39 -0300

    Backend Produto
    Ainda finalizar etapa de cadastro e pesquisa

--- application/php/cadastrar_produto.php
+++ application/php/cadastrar_produto.php
@@ -14,19 +14,46 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
         if (move_uploaded_file($_FILES["imagem"]["tmp_name"], $caminhoCompleto)) {
           
             //PEGAR CAMPOS DO FORM
-            $nomeProduto = $_POST["nome_produto"];
+            $nome_produto = $_POST["nome_produto"];
             $descricao = $_POST["descricao"];
             $fornecedor = $_POST["id_fornecedor"];
             $categoria = $_POST["categoria"];
             $genero = $_POST["id_genero"];
-            $imagem = $_POST["imagem"];
+            $imagem = $caminhoCompleto;
             
+            $query_nome = "SELECT * FROM tb_produto WHERE nome_produto = '".$nome_produto."';";
+
+            $conf_nome = "N";
+
+            $result_nome = mysqli_query($conn, $query_nome);
+
+            if($result_nome->num_rows > 0){
+                $conf_nome = "S";
+
+                if($conf_nome == "True"){
+                    echo "PRODUTO já cadastrado!";
+                    die();
+                }else{
+                    query_produto: "INSERT INTO tb_produto(nome_produto, descricao, fornecedor,categoria, genero, imagem) 
+                    VALUES('".$nome_produto."', '".$descricao."', '".$fornecedor."', '".$categoria."','".$genero."','".$imagem."') ;";
+
+                    if(mysqli_query($conn, $query_produto)){
+                        echo "True";
+                    }
+                } 
+
+            }
 
             //CRIAR A QUERY
-            $query_produto = "SELECT * FROM tb_produto;";
+            $query_produto = "INSERT INTO tb_produto(nome_produto, descricao, id_fornecedor, categoria, if_genero, local_img) 
+            VALUES('".$nome_produto."','".$descricao."', '".$fornecedor."', '".$categoria."', '".$genero."', '".$imagem."');";
 
             // FAZER CONFERENCIA E DEPOIS O INSERT NO BANCO
-            $result_produto = mysqli_query($conn, $query_produto);
+            if (mysqli_query($conn, $query_produto)){
+                echo "True";
+            }else{
+                mysqli_error($conn);
+            }
 
             
         } else {            
