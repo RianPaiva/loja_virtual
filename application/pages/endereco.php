be1964128c9f95ae35523449e82b1645e4c77957
gabriel lavechia <39846771+glavechia@users.noreply.github.com>
2023-11-15 13:49:10 -0300

    Fix

--- application/pages/endereco.php
+++ application/pages/endereco.php
@@ -25,22 +25,25 @@ include("../header_footer/header.php");
             <div class="row mt-5">
                 <div class="col-lg-12">
 
-                    <div class="card">
+                    <div class="card text-start">
                         <div class="card-body">
 
-                            <div class="row">
+                            <div class="row mx-auto">
                                 <label class="radio">
                                     <input type="radio" name="endereco">
                                     <div class="col-md-12">
+
+
                                         <div class="row text-start">
-                                            <h4>Rua SLA das Quantas</H4>
+                                            <h4>Rua 1</H4>
                                         </div>
+
                                         <div class="row text-start">
                                             <div class="col-md-3">
-                                                <H6>CEP : 1894572389575903089</h6>
+                                                <H6>CEP : 12345-678</h6>
                                             </div>
                                             <div class="col-md-3">
-                                                <H6>numero : 29</h6>
+                                                <H6> Numero: 1</h6>
                                             </div>
                                         </div>
                                     </div>
@@ -48,13 +51,137 @@ include("../header_footer/header.php");
                             </div>
                         </div>
                     </div>
+                </div>
+            </div>
+
+
+            <div class="row mt-3">
+                <div class="col-lg-12">
+
+                    <div class="card text-start">
+                        <div class="card-body">
+                            <div class="row mx-auto">
+                                <label class="radio">
+                                    <input type="radio" name="endereco">
+                                    <div class="col-md-12">
+                                        <div class="row text-start">
+                                            <h4>Rua 2</H4>
+                                        </div>
+                                        <div class="row text-start">
+                                            <div class="col-md-3">
+                                                <H6>CEP : 12345-678</h6>
+                                            </div>
+                                            <div class="col-md-3">
+                                                <H6> Numero: 1</h6>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </label>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="row mt-3">
+                <div class="col-lg-12">
+
+                    <div class="card text-start">
+                        <div class="card-body">
+                            <div class="row mx-auto">
+                                <label class="radio">
+                                    <input type="radio" name="endereco">
+                                    <div class="col-md-12">
+                                        <div class="row text-start">
+                                            <h4>Rua 3</H4>
+                                        </div>
+                                        <div class="row text-start">
+                                            <div class="col-md-3">
+                                                <H6>CEP : 12345-678</h6>
+                                            </div>
+                                            <div class="col-md-3">
+                                                <H6> Numero: 1</h6>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </label>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="row mt-3 text-start">
+                <div class="col-lg-12">
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="row mx-auto">
+                                <label class="radio">
+                                    <input type="radio" name="endereco">
+                                    <div class="col-md-12">
+                                        <div class="row text-start">
+                                            <h4>Rua 4</H4>
+                                        </div>
+                                        <div class="row text-start">
+                                            <div class="col-md-3">
+                                                <H6>CEP : 12345-678</h6>
+                                            </div>
+                                            <div class="col-md-3">
+                                                <H6> Numero: 1</h6>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </label>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Botão para acionar modal -->
+                <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#enderecos">
+                    Meus Endereços Salvos
+                </button>
+
+                <div id="enderecos" class="modal">
+
+                    <div class="modal-dialog">
+
+                        <div class="modal-content">
+
+                            <div class="modal-header">
+                                <h3 class="modal-title fs-5" id="enderecos">Endereços</h3>
+                                <button type="button" class="btn btn-close" data-bs-dismiss="modal"></button>
+                            </div>
+
+                        </div>
+
+                        <div class="modal-body">
+                            <p> blabla </p>
+                            <p> blabla </p>
+                            <p> blabla </p>
+                            <p> blabla </p>
+                        </div>
+
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
+                            <button type="button" class="btn btn-primary">Salvar mudanças</button>
+                        </div>
+                    </div>
 
                 </div>
 
+
+
+
             </div>
 
+
+
+
             <div class="row">
-                <div class="col-lg-12 mt-5" style="border:1px solid black">
+                <div class="col-lg-12 mt-4" style="border:1px solid black">
                     <h2> Cadastrar Novo Endereço </h2>
                 </div>
 
@@ -98,15 +225,39 @@ include("../header_footer/header.php");
                                 <input type="text" class="form-control" id="estado" placeholder="">
                             </div>
                         </div>
-
                 </div>
             </div>
+
         </div>
 
 
-        <div class="col-md-3" style="border:1px solid black">
-            <h1> kkkk </h1>
-        </div>
+        <aside class="col-md-3" style="border:1px solid black">
+            <div class="box rounded">
+                <header class="text-center">Resumo da compra</header>
+
+                <div class="card" style="width: 18rem;">
+                    <img class="card-img-top" src="..\imagens\tenis-nike-dunk-low-retro-cargo-khaki-mystic-red.png"
+                        alt="Imagem de capa do card">
+                    <div class="card-body">
+                        <h3 class="card-title"> Airforce X </h3>
+                        <p class="card-text">Quantidade: 1</p>
+                    </div>
+                </div>
+
+
+                <div class="info">
+                    <div><span>Sub-total</span><span>R$418,00</span></div>
+                    <div><span>Frete</span><span>R$0,00</span></div>
+
+                </div>
+
+                <footer class="rounded bg-color">
+                    <span>Total</span>
+                    <span>R$ 418</span>
+                </footer>
+            </div>
+            <button class="rounded">Confirmar Compra</button>
+        </aside>
 
     </div>
 
