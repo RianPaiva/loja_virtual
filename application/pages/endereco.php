49009edd674cf971c8e25c2c2e5aafcdf099283b
Vinicius Gama <vini.gama741@gmail.com>
2023-11-15 16:17:45 -0300

    Tela de endereço semi pronta

--- application/pages/endereco.php
+++ application/pages/endereco.php
@@ -2,7 +2,7 @@
 include("../header_footer/header.php");
 ?>
 
-<link href="../css/style_carrinho.css" rel="stylesheet">
+<link href="../css/style_endereco.css" rel="stylesheet">
 
 
 <div class="container-fluid mt-5">
@@ -19,206 +19,207 @@ include("../header_footer/header.php");
 
     <div class="row text-center justify-content-around">
 
-        <div class="col-md-7" style="border:1px solid black">
+        <div class="col-md-7">
             <h2> Endereços Salvos </h2>
 
-            <div class="row mt-5">
-                <div class="col-lg-12">
+            <div class="row row-custom overflow-y-scroll">
+                <div class="row mt-5">
+                    <div class="col-lg-12">
 
-                    <div class="card text-start">
-                        <div class="card-body">
+                        <div class="card text-start">
+                            <div class="card-body">
 
-                            <div class="row mx-auto">
-                                <label class="radio">
-                                    <input type="radio" name="endereco">
-                                    <div class="col-md-12">
-
-
-                                        <div class="row text-start">
-                                            <h4>Rua 1</H4>
+                                <div class="row mx-auto">
+                                    <label class="radio">
+                                        <div class="col">
+                                            <input type="radio" name="endereco">
                                         </div>
+                                        <div class="col-md-10">
 
-                                        <div class="row text-start">
-                                            <div class="col-md-3">
-                                                <H6>CEP : 12345-678</h6>
+
+                                            <div class="row text-start">
+                                                <h4>Rua 1</H4>
                                             </div>
-                                            <div class="col-md-3">
-                                                <H6> Numero: 1</h6>
+
+                                            <div class="row text-start">
+                                                <div class="col-md-3">
+                                                    <H6>CEP : 12345-678</h6>
+                                                </div>
+                                                <div class="col-md-3">
+                                                    <H6> Numero: 1</h6>
+                                                </div>
                                             </div>
                                         </div>
-                                    </div>
-                                </label>
+                                    </label>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
 
+                <div class="row mt-3">
+                    <div class="col-lg-12">
 
-            <div class="row mt-3">
-                <div class="col-lg-12">
-
-                    <div class="card text-start">
-                        <div class="card-body">
-                            <div class="row mx-auto">
-                                <label class="radio">
-                                    <input type="radio" name="endereco">
-                                    <div class="col-md-12">
-                                        <div class="row text-start">
-                                            <h4>Rua 2</H4>
-                                        </div>
-                                        <div class="row text-start">
-                                            <div class="col-md-3">
-                                                <H6>CEP : 12345-678</h6>
+                        <div class="card text-start">
+                            <div class="card-body">
+                                <div class="row mx-auto">
+                                    <label class="radio">
+                                        <input type="radio" name="endereco">
+                                        <div class="col-md-12">
+                                            <div class="row text-start">
+                                                <h4>Rua 2</H4>
                                             </div>
-                                            <div class="col-md-3">
-                                                <H6> Numero: 1</h6>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </label>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-
-            <div class="row mt-3">
-                <div class="col-lg-12">
-
-                    <div class="card text-start">
-                        <div class="card-body">
-                            <div class="row mx-auto">
-                                <label class="radio">
-                                    <input type="radio" name="endereco">
-                                    <div class="col-md-12">
-                                        <div class="row text-start">
-                                            <h4>Rua 3</H4>
-                                        </div>
-                                        <div class="row text-start">
-                                            <div class="col-md-3">
-                                                <H6>CEP : 12345-678</h6>
-                                            </div>
-                                            <div class="col-md-3">
-                                                <H6> Numero: 1</h6>
+                                            <div class="row text-start">
+                                                <div class="col-md-3">
+                                                    <H6>CEP : 12345-678</h6>
+                                                </div>
+                                                <div class="col-md-3">
+                                                    <H6> Numero: 1</h6>
+                                                </div>
                                             </div>
                                         </div>
-                                    </div>
-                                </label>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-
-            <div class="row mt-3 text-start">
-                <div class="col-lg-12">
-                    <div class="card">
-                        <div class="card-body">
-                            <div class="row mx-auto">
-                                <label class="radio">
-                                    <input type="radio" name="endereco">
-                                    <div class="col-md-12">
-                                        <div class="row text-start">
-                                            <h4>Rua 4</H4>
-                                        </div>
-                                        <div class="row text-start">
-                                            <div class="col-md-3">
-                                                <H6>CEP : 12345-678</h6>
-                                            </div>
-                                            <div class="col-md-3">
-                                                <H6> Numero: 1</h6>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </label>
+                                    </label>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
 
 
-                <div class="d-grid gap-2 d-md-block ">
+                <div class="row mt-3">
+                    <div class="col-lg-12">
 
-                    <button type="button" class="btn btn-primary mt-3" data-toggle="" data-target="#enderecos">
-                        Novo Cadastro
-                    </button>
-                    <button type="button" class="btn btn-primary mt-3" data-toggle="" data-target="#enderecos">
-                        Avançar
-                    </button>
+                        <div class="card text-start">
+                            <div class="card-body">
+                                <div class="row mx-auto">
+                                    <label class="radio">
+                                        <input type="radio" name="endereco">
+                                        <div class="col-md-12">
+                                            <div class="row text-start">
+                                                <h4>Rua 3</H4>
+                                            </div>
+                                            <div class="row text-start">
+                                                <div class="col-md-3">
+                                                    <H6>CEP : 12345-678</h6>
+                                                </div>
+                                                <div class="col-md-3">
+                                                    <H6> Numero: 1</h6>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </label>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
 
+
+                <div class="row mt-3 text-start">
+                    <div class="col-lg-12">
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="row mx-auto">
+                                    <label class="radio">
+                                        <input type="radio" name="endereco">
+                                        <div class="col-md-12">
+                                            <div class="row text-start">
+                                                <h4>Rua 4</H4>
+                                            </div>
+                                            <div class="row text-start">
+                                                <div class="col-md-3">
+                                                    <H6>CEP : 12345-678</h6>
+                                                </div>
+                                                <div class="col-md-3">
+                                                    <H6> Numero: 1</h6>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </label>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
 
+            <div class="row mt-5 mb-5">
 
-
-
-            <div class="row">
-                <div class="col-lg-12 mt-4" style="border:1px solid black">
-                    <h2> Cadastrar Novo Endereço </h2>
+                <div class="col-md-4">
+                    <p class="">
+                        <a class="btn btn-primary new-cad" data-bs-toggle="collapse" href="#multiCollapseExample1" role="button" aria-expanded="false" aria-controls="multiCollapseExample1">Novo Endereço</a>
+                    </p>
                 </div>
 
                 <div class="row">
-                    <form class="row justify-content-center">
+                    <div class="col">
+                        <div class="collapse multi-collapse" id="multiCollapseExample1">
+                            <div class="card card-body">
+                                <div class="row">
+                                    <div class="col-lg-12 mt-4">
+                                        <h2> Cadastrar Novo Endereço </h2>
+                                    </div>
 
-                        <div class="row justify-content-center mt-3">
-                            <div class="col-md-6">
-                                <label for="rua" class="form-label">Rua</label>
-                                <input type="text" class="form-control" id="rua">
+                                    <div class="row">
+                                        <form class="row justify-content-center">
+
+                                            <div class="row justify-content-center mt-3">
+                                                <div class="col-md-6 text-start">
+                                                    <label for="rua" class="form-label">Rua</label>
+                                                    <input type="text" class="form-control" id="rua">
+                                                </div>
+
+                                                <div class="col-md-6 text-start">
+                                                    <label for="bairro" class="form-label">Bairro</label>
+                                                    <input type="text" class="form-control" id="bairro">
+                                                </div>
+
+                                            </div>
+
+                                            <div class="row justify-content-center">
+                                                <div class="col-md-6 text-start">
+                                                    <label for="cidade" class="form-label">Cidade</label>
+                                                    <input type="text" class="form-control" id="cidade" placeholder="">
+                                                </div>
+
+                                                <div class="col-md-6 text-start">
+                                                    <label for="numero" class="form-label">Numero</label>
+                                                    <input type="text" class="form-control" id="numero" placeholder="">
+                                                </div>
+                                            </div>
+
+                                            <div class="row justify-content-center">
+
+                                                <div class="col-md-6 text-start">
+                                                    <label for="complemento" class="form-label">Complemento</label>
+                                                    <input type="text" class="form-control" id="complemento" placeholder="">
+                                                </div>
+
+                                                <div class="col-md-6 text-start">
+                                                    <label for="estado" class="form-label">Estado</label>
+                                                    <input type="text" class="form-control" id="estado" placeholder="">
+                                                </div>
+                                            </div>
+
+                                            <div class="row">
+                                                <div class="comprar-button d-flex justify-content-center mb-4 mt-4">
+                                                    <input type="submit" class="btn btn-warning cadastrar" id="btn_comprar" value="Cadastrar">
+                                                </div>
+                                            </div>
+                                    </div>
+                                </div>
                             </div>
-
-                            <div class="col-md-6">
-                                <label for="bairro" class="form-label">Bairro</label>
-                                <input type="text" class="form-control" id="bairro">
-                            </div>
-
                         </div>
+                    </div>
 
-                        <div class="row justify-content-center">
-                            <div class="col-md-6">
-                                <label for="cidade" class="form-label">Cidade</label>
-                                <input type="text" class="form-control" id="cidade" placeholder="">
-                            </div>
-
-                            <div class="col-md-6">
-                                <label for="numero" class="form-label">Numero</label>
-                                <input type="text" class="form-control" id="numero" placeholder="">
-                            </div>
-                        </div>
-
-                        <div class="row justify-content-center">
-
-                            <div class="col-md-6">
-                                <label for="complemento" class="form-label">Complemento</label>
-                                <input type="text" class="form-control" id="complemento" placeholder="">
-                            </div>
-
-                            <div class="col-md-6">
-                                <label for="estado" class="form-label">Estado</label>
-                                <input type="text" class="form-control" id="estado" placeholder="">
-                            </div>
-                        </div>
                 </div>
             </div>
-
         </div>
 
 
-        <aside class="col-md-3 text-center" style="border:1px solid black">
+        <aside class="col-md-3 text-center">
             <div class="box rounded">
-                <header class="text-center">Resumo da compra</header>
-
-                <div class="card" style="width: 18rem;">
-                    <img class="card-img-top" src="..\imagens\tenis-nike-dunk-low-retro-cargo-khaki-mystic-red.png"
-                        alt="Imagem de capa do card">
-                    <div class="card-body">
-                        <h3 class="card-title"> Airforce X </h3>
-                        <p class="card-text">Quantidade: 1</p>
-                    </div>
-                </div>
-
+                <header class="text-center">Resumo da compra</header>>
 
                 <div class="info">
                     <div><span>Sub-total</span><span>R$418,00</span></div>
@@ -231,13 +232,19 @@ include("../header_footer/header.php");
                     <span>R$ 418</span>
                 </footer>
             </div>
-            <button class="rounded">Confirmar Compra</button>
+            <div class="row">
+                <div class="comprar-button d-flex justify-content-center mb-4 mt-4">
+                    <input type="submit" class="btn btn-warning avancar" id="btn_avancar" value="Avançar">
+                </div>
+            </div>
         </aside>
 
     </div>
 
 
-
+    <div class="row">
+        <hr class="opacity-0" style="border: 45px solid transparent">
+    </div>
 
 
 
