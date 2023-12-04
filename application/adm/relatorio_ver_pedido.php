3a787cd27cecb632dcc1c5606ced28deb1a5afb2
Vinicius Gama <vini.gama741@gmail.com>
2023-12-04 12:10:35 -0300

    Ajuste dos arquivos

--- application/adm/relatorio_ver_pedido.php
+++ application/adm/relatorio_ver_pedido.php
@@ -12,7 +12,7 @@ include("../header_footer/header.php");
     <title> Meu Carrinho </title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <link href="../css/style_carrinho.css" rel="stylesheet">
+    <link href="../css/style_relatorio_ver_pedido.css" rel="stylesheet">
     <link href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css" rel="stylesheet" />
 
 </head>
